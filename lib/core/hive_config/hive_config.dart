94af31582836be0cdbbfeb7aace022c98c15a25d
jt trinh <Nhattd@selex.vn>
2023-11-09 13:37:33 +0700

    convert convinient name

--- lib/core/hive_config/hive_config.dart
+++ lib/core/hive_config/hive_config.dart
@@ -1,20 +1,22 @@
-class HiveConstants {
-  HiveConstants._();
-  static const int bmi = 1;
-  static const int bloodSugar = 2;
-  static const int bloodPressure = 3;
-}
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:your_healthy/core/hive_config/hive_constants.dart';
+import 'package:your_healthy/domain/models/bmi_model.dart';
 
-class HiveKey {
-  HiveKey._();
-  static const String bmiModel = "bmiModel";
-  static const String bloodSugarModel = "bloodSugarModel";
-  static const String bloodPressureModel = "bloodPressureModel";
-}
+class HiveConfig {
+  late Box<BMIModel> bmiBox;
 
-class HiveBox {
-  HiveBox._();
-  static const String bmiBox = "bmiBox";
-  static const String bloodSugarBox = "bloodSugarBox";
-  static const String bloodPressureBox = "bloodPressureBox";
+  Future<void> init() async {
+    final appDocumentDirectory =
+        await path_provider.getApplicationDocumentsDirectory();
+    Hive.init(appDocumentDirectory.path);
+
+    //Handle bmi hive box
+    Hive.registerAdapter(BMIModelAdapter());
+    bmiBox = await Hive.openBox(HiveBox.bmiBox);
+  }
+
+  void dispose() {
+    Hive.close();
+  }
 }
--- lib/core/hive_config/hive_constants.dart
+++ lib/core/hive_config/hive_constants.dart
@@ -1,22 +1,20 @@
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
-import 'package:your_healthy/core/hive_config/hive_config.dart';
-import 'package:your_healthy/domain/models/bmi_model.dart';
+class HiveConstants {
+  HiveConstants._();
+  static const int bmi = 1;
+  static const int bloodSugar = 2;
+  static const int bloodPressure = 3;
+}
 
-class HiveConfig {
-  late Box<BMIModel> bmiBox;
+class HiveKey {
+  HiveKey._();
+  static const String bmiModel = "bmiModel";
+  static const String bloodSugarModel = "bloodSugarModel";
+  static const String bloodPressureModel = "bloodPressureModel";
+}
 
-  Future<void> init() async {
-    final appDocumentDirectory =
-        await path_provider.getApplicationDocumentsDirectory();
-    Hive.init(appDocumentDirectory.path);
-
-    //Handle bmi hive box
-    Hive.registerAdapter(BMIModelAdapter());
-    bmiBox = await Hive.openBox(HiveBox.bmiBox);
-  }
-
-  void dispose() {
-    Hive.close();
-  }
+class HiveBox {
+  HiveBox._();
+  static const String bmiBox = "bmiBox";
+  static const String bloodSugarBox = "bloodSugarBox";
+  static const String bloodPressureBox = "bloodPressureBox";
 }
