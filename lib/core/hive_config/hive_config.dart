d976971c0061ed92e948ede1ba918fbc738186b9
jt trinh <Nhattd@selex.vn>
2023-11-09 13:36:26 +0700

    blood pressure model

--- lib/core/hive_config/hive_config.dart
+++ lib/core/hive_config/hive_config.dart
@@ -2,16 +2,19 @@ class HiveConstants {
   HiveConstants._();
   static const int bmi = 1;
   static const int bloodSugar = 2;
+  static const int bloodPressure = 3;
 }
 
 class HiveKey {
   HiveKey._();
   static const String bmiModel = "bmiModel";
   static const String bloodSugarModel = "bloodSugarModel";
+  static const String bloodPressureModel = "bloodPressureModel";
 }
 
 class HiveBox {
   HiveBox._();
   static const String bmiBox = "bmiBox";
   static const String bloodSugarBox = "bloodSugarBox";
+  static const String bloodPressureBox = "bloodPressureBox";
 }
---
+++ lib/domain/models/blood_pressure_model.dart
@@ -0,0 +1,27 @@
+import 'package:hive/hive.dart';
+import 'package:your_healthy/core/hive_config/hive_config.dart';
+
+part 'blood_pressure_model.g.dart';
+
+@HiveType(typeId: HiveConstants.bloodPressure)
+class BloodPressureModel extends HiveObject {
+  @override
+  @HiveField(0)
+  String? key;
+  @HiveField(1)
+  int? systolic;
+  @HiveField(3)
+  int? diastolic;
+  @HiveField(4)
+  int? type;
+  @HiveField(5)
+  int? dateTime;
+
+  BloodPressureModel({
+    this.key,
+    this.dateTime,
+    this.systolic,
+    this.diastolic,
+    this.type,
+  });
+}
